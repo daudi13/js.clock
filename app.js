0183b970d448475b00ee4bfbc2d907e21281dfbc
daudi13 <davouma13@gmail.com>
2021-06-17 06:07:35 +0300

    add dark/light mode

---
+++ app.js
@@ -0,0 +1,22 @@
+const hourEl = document.querySelector('.hour')
+const minuteEl = document.querySelector('.minute')
+const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+const toggleEl = document.querySelector('.toggle')
+
+const days = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+const months = new Array("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+
+toggleEl.addEventListener('click', (e) => {
+    const html = document.querySelector('html')
+
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark')
+        e.target.innerHTML = 'Dark mode'
+    } else {
+        html.classList.add('dark')
+        e.target.innerHTML = 'Light mode'
+    }
+})
